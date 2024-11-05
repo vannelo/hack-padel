4fe4608c04c4b87d8acf8d7ed5f19f4abe823797
Vannelo <work@vannelo.com>
2024-11-05 12:10:11 -0600

    Add dummy score table

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,118 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // Dummy data for the tournament
+  const couples = [
+    "Vannelo / Diego",
+    "Yira / Monse",
+    "Ivan / Christian",
+    "Cesar / Limón",
+    "Ivan / Ariel",
+    "Pablo / Javi",
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const scores = [
+    [null, 6, 6, 6, 2, 6],
+    [2, null, 6, 5, 6, 3],
+    [3, 4, null, 6, 2, 6],
+    [4, 6, 3, null, 6, 2],
+    [6, 2, 4, 6, null, 5],
+    [2, 6, 3, 6, 6, null],
+  ];
+
+  const currentMatchNumber = 4;
+  const currentLeader = "Vannelo / Diego";
+
+  // Calculate total scores for each couple
+  const totalScores = scores.map((row) =>
+    row.reduce(
+      (total: number, score) => (score !== null ? total + score : total),
+      0
+    )
+  );
+
+  return (
+    <main className="min-h-[100vh] flex justify-center items-center">
+      <Image
+        src="/img/home-bg.jpg"
+        alt="Padel court"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className="absolute inset-0 bg-black opacity-80 z-10"></div>
+      <div className="absolute inset-0 z-20 min-h-[100vh] flex justify-center items-center">
+        {/* HEADING */}
+        <div className="flex flex-col items-center">
+          <h1 className="text-primary text-6xl font-bold tracking-tighter">
+            HACK PADEL
+          </h1>
+          <h2 className="font-bold tracking-tighter uppercase text-2xl">
+            Tournament
+          </h2>
+          {/* SCORES */}
+          <div className="mt-4 uppercase">
+            <div className="flex justify-center items-center gap-4 mb-4">
+              <h3 className="text-xl font-bold">
+                Líderes: <span className="text-primary">{currentLeader}</span>
+              </h3>
+              <h4 className="text-lg font-bold">
+                Partido:{" "}
+                <span className="text-primary font-sans">
+                  {currentMatchNumber}/{couples.length - 1}
+                </span>
+              </h4>
+            </div>
+
+            <table className="table-auto border-collapse border border-gray-400 font-bold w-full text-2xl">
+              <thead>
+                <tr>
+                  <th className="border border-gray-400 p-2 text-primary">
+                    Parejas
+                  </th>
+                  {couples.map((couple, index) => (
+                    <th
+                      key={index}
+                      className="border border-gray-400 p-2 text-lg"
+                    >
+                      {couple}
+                    </th>
+                  ))}
+                  <th className="border border-gray-400 p-2 text-primary">
+                    Puntos
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {scores.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    <td className="border border-gray-400 p-2 font-bold text-lg">
+                      {couples[rowIndex]}
+                    </td>
+                    {row.map((score, colIndex) => (
+                      <td
+                        key={colIndex}
+                        className="border border-gray-400 p-2 text-center font-sans text-3xl"
+                      >
+                        {score === 6 ? (
+                          <span className="text-primary">{score}</span>
+                        ) : score !== null ? (
+                          score
+                        ) : (
+                          "-"
+                        )}
+                      </td>
+                    ))}
+                    <td className="border border-gray-400 p-2 text-center font-bold font-sans">
+                      {totalScores[rowIndex]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
