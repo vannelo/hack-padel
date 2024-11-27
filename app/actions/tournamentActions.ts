123da840942527cba3475ba1d97dbc1202f03d3e
Vannelo <work@vannelo.com>
2024-11-26 20:28:36 -0600

    update cache

--- app/actions/tournamentActions.ts
+++ app/actions/tournamentActions.ts
@@ -4,50 +4,55 @@ import { tournamentService } from "@/domain";
 import { Tournament } from "@/domain/models/Tournament";
 import { Couple } from "@/domain/models/Couple";
 import { revalidatePath } from "next/cache";
+import { cache } from "react";
 
-export async function createTournament(
-  name: string,
-  courts: number,
-  couples: Couple[],
-): Promise<Tournament> {
-  const newTournament = await tournamentService.createTournament(
-    name,
-    courts,
-    couples,
-  );
-  revalidatePath("/torneos");
-  return newTournament;
-}
-
-export async function getAllTournaments(): Promise<Tournament[]> {
-  return await tournamentService.getAllTournaments();
-}
-
-export async function getTournamentById(
-  id: string,
-): Promise<Tournament | null> {
-  return await tournamentService.getTournamentById(id);
-}
-
-export async function updateMatchResults(
-  tournamentId: string,
-  roundId: string,
-  matchResults: {
-    [key: string]: { couple1Score: number; couple2Score: number };
+export const createTournament = cache(
+  async (
+    name: string,
+    courts: number,
+    couples: Couple[],
+  ): Promise<Tournament> => {
+    const newTournament = await tournamentService.createTournament(
+      name,
+      courts,
+      couples,
+    );
+    revalidatePath("/torneos");
+    return newTournament;
   },
-): Promise<void> {
-  await tournamentService.updateMatchResults(
-    tournamentId,
-    roundId,
-    matchResults,
-  );
-  revalidatePath(`/torneos/${tournamentId}`);
-}
+);
 
-export async function endRound(
-  tournamentId: string,
-  roundId: string,
-): Promise<void> {
-  await tournamentService.endRound(tournamentId, roundId);
-  revalidatePath(`/torneos/${tournamentId}`);
-}
+export const getAllTournaments = cache(async (): Promise<Tournament[]> => {
+  const tournaments = await tournamentService.getAllTournaments();
+  return tournaments;
+});
+
+export const getTournamentById = cache(
+  async (id: string): Promise<Tournament | null> => {
+    return await tournamentService.getTournamentById(id);
+  },
+);
+
+export const updateMatchResults = cache(
+  async (
+    tournamentId: string,
+    roundId: string,
+    matchResults: {
+      [key: string]: { couple1Score: number; couple2Score: number };
+    },
+  ): Promise<void> => {
+    await tournamentService.updateMatchResults(
+      tournamentId,
+      roundId,
+      matchResults,
+    );
+    revalidatePath(`/torneos/${tournamentId}`);
+  },
+);
+
+export const endRound = cache(
+  async (tournamentId: string, roundId: string): Promise<void> => {
+    await tournamentService.endRound(tournamentId, roundId);
+    revalidatePath(`/torneos/${tournamentId}`);
+  },
+);
